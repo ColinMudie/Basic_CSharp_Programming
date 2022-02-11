e2c37cd6df7da5c152e76600271e63f643c93a55
Colin Mudie <csmudie1@gmail.com>
2022-02-11 16:05:12 -0500

    Initialize Assignment 1

---
+++ Assignment_1/Assignment1/TechWorldBL/Instructor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechWorldBL
+{
+    public class Instructor
+    {
+        public Instructor()
+        {
+
+        }
+
+        public Instructor(string p_instructorName, float p_avgFeedback, int p_experience, string[] p_instructorSkill)
+        {
+            this.AvgFeedback = p_avgFeedback;
+            this.Experience = p_experience;
+            this.InstructorName = p_instructorName;
+            this.InstructorSkill = p_instructorSkill;
+        }
+
+        public float AvgFeedback { get; set; }
+        public int Experience { get; set; }
+        public string InstructorName { get; set; }
+        public string[] InstructorSkill { get; set; }
+
+        /// <summary>
+        /// This method accepts a technology name as input parameter and
+        /// check whether the instructor possesses that skill.
+        /// </summary>
+        /// <param name="p_technology">technology to check</param>
+        /// <returns>boolean result</returns>
+        public bool CheckSkill(string p_technology)
+        {
+            if (!this.ValidateEligibility())
+            {
+                return false;
+            }
+
+            bool result = false;
+            foreach(string skill in this.InstructorSkill)
+            {
+                if (skill.ToLower() == p_technology.ToLower())
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// The method should check the eligibility criteria as given below:
+        /// 
+        ///     - An instructor with more than 3 years of experience should have an 
+        ///         average feedback greater than or equal to 4.5.
+        ///         
+        ///     - An instructor with less than or equal to 3 years of experience 
+        ///         should have an average feedback greater than or equal to 4.
+        /// </summary>
+        /// <returns>boolean result</returns>
+        public bool ValidateEligibility()
+        {
+            if (this.Experience > 3 && this.AvgFeedback >= 4.5)
+            {
+                return true;
+            }
+            else if (this. Experience <= 3 && this.AvgFeedback >= 4)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Assignment_1/Assignment1/TechWorldBLTests/InstructorTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechWorldBL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechWorldBL.Tests
+{
+    [TestClass()]
+    public class InstructorTests
+    {
+        [TestMethod()]
+        public void CheckSkillTest()
+        {
+            Instructor _instructor1 = new Instructor("Colin", 5, 3, new string[]{ "SQL", "C#", "Angular"});
+
+            // Test 1
+            bool result = _instructor1.CheckSkill("SQL");
+            Assert.IsTrue(result);
+
+            // Test 2
+            result = _instructor1.CheckSkill("Jenkins");
+            Assert.IsFalse(result);
+
+            // Test 3 - should check eligibility
+            _instructor1 = new Instructor("Bad Trainer", 1, 2, new string[] { "SQL", "C#", "Angular" });
+            result = _instructor1.CheckSkill("SQL");
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod()]
+        public void ValidateEligibilityTest()
+        {
+            // Test 1
+            Instructor _instructor1 = new Instructor("Colin", 5, 3, new string[] { "SQL", "C#", "Angular" });
+            bool result = _instructor1.ValidateEligibility();
+            Assert.IsTrue(result);
+
+            // Test 2
+            _instructor1 = new Instructor("Bad Trainer", 1, 2, new string[] { "Word", "Excel" });
+            result = _instructor1.ValidateEligibility();
+            Assert.IsFalse(result);
+
+            
+
+
+        }
+
+        [TestMethod()]
+        public void InstructorTest()
+        {
+            Instructor _instructor1 = new Instructor("Colin", 5, 3, new string[] { "SQL", "C#", "Angular" });
+            Instructor _instructor2 = new Instructor("Carl", 7, 4, new string[] { "Java", "React", "Spring" });
+            Assert.AreNotEqual(_instructor1, _instructor2);
+        }
+    }
+}

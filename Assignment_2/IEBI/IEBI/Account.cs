332a86e697b427d53c2d3b87eddc1cb6d171e5d4
Colin Mudie <csmudie1@gmail.com>
2022-02-14 17:37:09 -0500

    solved assignment 2

---
+++ Assignment_2/IEBI/IEBI/Account.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEBI
+{
+    public class Account
+    {
+        public long AccountNo { get; set; }
+        public double Balance { get; set; }
+        public string[] Payees { get; set; }
+        public long[] PayeesAccount { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(long p_accountNo, double p_balance)
+        {
+            this.AccountNo = p_accountNo;
+            this.Balance = p_balance;
+        }
+
+        public Account(long p_accountNo, double p_balance, string[] p_payees, long[] p_payeesAccount)
+        {
+            this.AccountNo = p_accountNo;
+            this.Balance = p_balance;
+            this.Payees = p_payees;
+            this.PayeesAccount = p_payeesAccount;
+        }
+
+        public int DebitAmount(double p_amount)
+        {
+            if (p_amount <= this.Balance)
+            {
+                this.Balance -= p_amount;
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int TransferMoney(long p_payeeAccountNo, double p_amount)
+        {
+            foreach(long acc in this.PayeesAccount)
+            {
+                if (acc == p_payeeAccountNo)
+                {
+                    return DebitAmount(p_amount);
+                }
+            }
+            return -1;
+        }
+
+        public int TransferMoney(string p_nickname, double p_amount)
+        {
+            foreach (string nickname in this.Payees)
+            {
+                if (nickname == p_nickname)
+                {
+                    return DebitAmount(p_amount);
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Assignment_2/IEBI/IEBITests/AccountTests.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IEBI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEBI.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        [TestMethod()]
+
+        public void CtorTest()
+        {
+            Account testA = new Account();
+            Account testB = new Account();
+            Assert.AreEqual(testA.AccountNo, testB.AccountNo);
+        }
+
+        // The constructor should initialize the ‘accountNo’ and ‘balance’
+        // member variables with appropriate values
+        [TestMethod()]
+        public void Ctor2ParamsTest()
+        {
+            long accNum = 10;
+            double balance = 5.27;
+            Account test = new Account(accNum, balance);
+            Assert.AreEqual(accNum, test.AccountNo);
+            Assert.AreEqual(balance, test.Balance);
+        }
+
+        // The constructor should initialize ‘payees’ and ‘payeesAccount’
+        // member variables. ‘accountNo’ and ‘balance’ member variables
+        // should be initialized by calling the appropriate constructor
+        [TestMethod()]
+        public void Ctor4ParamsTest()
+        {
+            long accNum = 10;
+            double balance = 5.27;
+            string[] payees = { "billy", "bob", "person" };
+            long[] payeesAcc = { 9, 10, 11 };
+            Account test = new Account(accNum, balance, payees, payeesAcc);
+            Assert.AreEqual(accNum, test.AccountNo);
+            Assert.AreEqual(balance, test.Balance);
+            Assert.AreEqual(payees, test.Payees);
+            Assert.AreEqual(payeesAcc, test.PayeesAccount);
+        }
+
+        // This method debits the amount from user’s account only if the
+        // amount is less than or equal to balance and returns 1 on success
+
+        // If there is no sufficient balance, the method should return 0
+        [TestMethod()]
+        public void DebitAmountTest()
+        {
+            long accNum = 10;
+            double balance = 5.27;
+            Account test = new Account(accNum, balance);
+
+            int result = test.DebitAmount(4.01);
+            Assert.AreEqual(1, result);
+            Assert.AreEqual(5.27 - 4.01, test.Balance);
+
+            result = test.DebitAmount(20.00);
+            Assert.AreEqual(0, result);
+        }
+
+        // Check if the payee’s account number is present
+        // in the available payees’ account list
+
+        // Call ‘DebitAmount’ method if the payee’s
+        // account number is present and return the return
+        // value of the DebitAmount method
+
+        // Return -1 if the account number is not present in the
+        // available payees’ account list
+        [TestMethod()]
+        public void TransferMoneyAccountNumberTest()
+        {
+            long accNum = 10;
+            double balance = 5.27;
+            string[] payees = { "billy", "bob", "person" };
+            long[] payeesAcc = { 9, 10, 11 };
+            Account test = new Account(accNum, balance, payees, payeesAcc);
+
+            // match and correct amt - pass
+            int result = test.TransferMoney(9, 4.00);
+            Assert.AreEqual(1, result);
+            Assert.AreEqual(5.27 - 4.00, test.Balance);
+
+            // match and incorrect amt - fail
+            test = new Account(accNum, balance, payees, payeesAcc);
+            result = test.TransferMoney(9, 20.00);
+            Assert.AreEqual(0, result);
+            Assert.AreEqual(5.27, test.Balance);
+
+            // incorrect match - fail
+            test = new Account(accNum, balance, payees, payeesAcc);
+            result = test.TransferMoney(4, 4.00);
+            Assert.AreEqual(-1, result);
+            Assert.AreEqual(5.27, test.Balance);
+        }
+
+        // Check if nickName is present in the available payees’ list
+
+        // Call ‘DebitAmount’ method if the payee’s name is
+        // present and return the return value of the DebitAmount method
+
+        // Return -1 if the name is not present in the
+        // available payees’ name list
+        [TestMethod()]
+        public void TransferMoneyNicknameTest()
+        {
+            long accNum = 10;
+            double balance = 5.27;
+            string[] payees = { "billy", "bob", "person" };
+            long[] payeesAcc = { 9, 10, 11 };
+            Account test = new Account(accNum, balance, payees, payeesAcc);
+
+            // match and correct amt - pass
+            int result = test.TransferMoney("billy", 4.00);
+            Assert.AreEqual(1, result);
+            Assert.AreEqual(5.27 - 4.00, test.Balance);
+
+            // match and incorrect amt - fail
+            test = new Account(accNum, balance, payees, payeesAcc);
+            result = test.TransferMoney("billy", 20.00);
+            Assert.AreEqual(0, result);
+            Assert.AreEqual(5.27, test.Balance);
+
+            // incorrect match - fail
+            test = new Account(accNum, balance, payees, payeesAcc);
+            result = test.TransferMoney("not match", 4.00);
+            Assert.AreEqual(-1, result);
+            Assert.AreEqual(5.27, test.Balance);
+        }
+    }
+}

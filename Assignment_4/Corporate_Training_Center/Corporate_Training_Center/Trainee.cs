74977e2c1b7f8de25ddf7e69ae8bbd339a389da5
Colin Mudie <csmudie1@gmail.com>
2022-02-15 14:50:17 -0500

    initialize assignment 5

---
+++ Assignment_4/Corporate_Training_Center/Corporate_Training_Center/Trainee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corporate_Training_Center
+{
+    public class Trainee
+    {
+        public string[] FocusArea { get; set; }
+        public float[] Marks { get; set; }
+
+        public Trainee()
+        {
+
+        }
+
+        public Trainee(string[] p_focusArea, float[] p_marks)
+        {
+
+        }
+
+        public float CalculateScore(out float percentage, out char grade, out string message, bool isRetest = false)
+        {
+            percentage = 0;
+            grade = 'A';
+            message = "good job!";
+            return 0;
+        }
+    }
+}

53376d90016afebb6156f45fb23621f93b1f3550
Colin Mudie <csmudie1@gmail.com>
2022-02-15 16:53:20 -0500

    solved assignment 7

--- Assignment_7/Corporada/Corporada/LoanCalculation.cs
+++ Assignment_7/Corporada/Corporada/LoanCalculation.cs
@@ -8,5 +8,66 @@ namespace Corporada
 {
     public class LoanCalculation
     {
+        public LoanCalculation()
+        {
+        }
+
+        public bool ApplyForLoan(float salary, out int loanAmount, out int interest, params string[] doc)
+        {
+            if(doc.Length > 1 && doc.Contains("Passport"))
+            {
+                if (salary < 12000)
+                {
+                    loanAmount = 25000;
+                    interest = 4;
+                }
+                else if (salary >= 12000 && salary <= 50000)
+                {
+                    loanAmount = 50000;
+                    interest = 6;
+                }
+                else
+                {
+                    loanAmount = 100000;
+                    interest = 8;
+                }
+                return true;
+            }
+
+            loanAmount = 0;
+            interest = 0;
+            return false;
+        }
+
+        public void Emi_Discount(int loanAmount, int relativeMarking, int tenure, int interest, out int emi)
+        {
+            emi = (loanAmount + (loanAmount * interest / 100));
+            int discount;
+            switch (relativeMarking)
+            {
+                case 1:
+                    discount = 10;
+                    break;
+
+                case 2:
+                    discount = 8;
+                    break;
+
+                case 3:
+                    discount = 6;
+                    break;
+
+                default:
+                    discount = 4;
+                    break;
+            }
+            emi -= emi * discount / 100;
+        }
+
+        public void SalaryDeduction(ref float salary, int relativeMarking, int loanAmount, int tenure, int interest, out int emi)
+        {
+            this.Emi_Discount(loanAmount, relativeMarking, tenure, interest, out emi);
+            salary -= emi;
+        }
     }
 }
---
+++ Assignment_7/Corporada/CorporadaTests/LoanCalculationTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Corporada;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corporada.Tests
+{
+    [TestClass()]
+    public class LoanCalculationTests
+    {
+        [TestMethod()]
+        public void ApplyForLoanTest()
+        {
+            LoanCalculation test = new LoanCalculation();
+            int loanAmount;
+            int interest;
+            bool result = test.ApplyForLoan(10000, out loanAmount, out interest, "Passport", "Drivers License");
+            Assert.IsTrue(result);
+            Assert.AreEqual(25000, loanAmount);
+            Assert.AreEqual(4, interest);
+
+            result = test.ApplyForLoan(13000, out loanAmount, out interest, "Passport", "Drivers License");
+            Assert.IsTrue(result);
+            Assert.AreEqual(50000, loanAmount);
+            Assert.AreEqual(6, interest);
+
+            result = test.ApplyForLoan(55000, out loanAmount, out interest, "Passport", "Drivers License");
+            Assert.IsTrue(result);
+            Assert.AreEqual(100000, loanAmount);
+            Assert.AreEqual(8, interest);
+
+            result = test.ApplyForLoan(10000, out loanAmount, out interest, "library card", "Drivers License");
+            Assert.IsFalse(result);
+
+            result = test.ApplyForLoan(10000, out loanAmount, out interest, "Passport");
+            Assert.IsFalse(result);
+
+
+        }
+
+        [TestMethod()]
+        public void Emi_DiscountTest()
+        {
+            LoanCalculation test = new LoanCalculation();
+            int loanAmount;
+            int interest;
+            test.ApplyForLoan(10000, out loanAmount, out interest, "Passport", "Drivers License");
+            int emi;
+            test.Emi_Discount(loanAmount, 1, 2, interest, out emi);
+
+            Assert.AreEqual(23400, emi);
+
+        }
+
+        [TestMethod()]
+        public void SalaryDeductionTest()
+        {
+            LoanCalculation test = new LoanCalculation();
+            int loanAmount;
+            int interest;
+            float salary = 10000;
+            bool result = test.ApplyForLoan(salary, out loanAmount, out interest, "Passport", "Drivers License");
+            int emi;
+            test.SalaryDeduction(ref salary, 1, loanAmount, 2, interest, out emi);
+            Assert.AreEqual(25000, salary);
+            Assert.AreEqual(23400, emi);
+        }
+
+        
+    }
+}

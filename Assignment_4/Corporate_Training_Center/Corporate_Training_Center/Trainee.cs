1370e66d4572f0beb6bd4a5d7efaaf4cd00376ca
Colin Mudie <csmudie1@gmail.com>
2022-02-15 15:19:34 -0500

    solved

--- Assignment_4/Corporate_Training_Center/Corporate_Training_Center/Trainee.cs
+++ Assignment_4/Corporate_Training_Center/Corporate_Training_Center/Trainee.cs
@@ -18,15 +18,49 @@ namespace Corporate_Training_Center
 
         public Trainee(string[] p_focusArea, float[] p_marks)
         {
-
+            this.FocusArea = p_focusArea;
+            this.Marks = p_marks;
         }
 
         public float CalculateScore(out float percentage, out char grade, out string message, bool isRetest = false)
         {
             percentage = 0;
-            grade = 'A';
-            message = "good job!";
-            return 0;
+            foreach (float mark in this.Marks)
+            {
+                percentage += mark;
+            }
+            percentage /= this.Marks.Length;
+            if (isRetest)
+            {
+                percentage -= 10;
+            }
+
+            if (percentage >= 85 && percentage <= 100)
+            {
+                grade = 'A';
+            }
+            else if (percentage >= 75 && percentage <= 84)
+            {
+                grade = 'B';
+            }
+            else if (percentage >= 0 && percentage < 75)
+            {
+                grade = 'C';
+            }
+            else 
+            {
+                grade = 'N';
+            }
+
+            if (percentage >= 50 && percentage <= 100)
+            {
+                message = "Qualified";
+            }
+            else
+            {
+                message = "Not Qualified";
+            }
+            return percentage;
         }
     }
 }
---
+++ Assignment_4/Corporate_Training_Center/Corporate_Training_CenterTests/TraineeTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Corporate_Training_Center;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corporate_Training_Center.Tests
+{
+    [TestClass()]
+    public class TraineeTests
+    {
+        [TestMethod()]
+        public void CalculateScoreTest()
+        {
+
+            Trainee test = new Trainee(new string[] { "SQL", "C#" }, new float[] { 85, 95, 93 });
+            float percentage;
+            char grade;
+            string message;
+
+
+            test.CalculateScore(out percentage, out grade, out message);
+            Assert.AreEqual(91, percentage);
+            Assert.AreEqual('A', grade);
+            Assert.AreEqual("Qualified", message);
+
+            //retest 
+            test.CalculateScore(out percentage, out grade, out message, true);
+            Assert.AreEqual(81, percentage);
+            Assert.AreEqual('B', grade);
+            Assert.AreEqual("Qualified", message);
+
+            // b - qualified
+            test = new Trainee(new string[] { "SQL", "C#" }, new float[] { 75, 76, 77 });
+            test.CalculateScore(out percentage, out grade, out message);
+            Assert.AreEqual(76, percentage);
+            Assert.AreEqual('B', grade);
+            Assert.AreEqual("Qualified", message);
+
+            // c - qualified
+            test = new Trainee(new string[] { "SQL", "C#" }, new float[] { 50, 51, 52 });
+            test.CalculateScore(out percentage, out grade, out message);
+            Assert.AreEqual(51, percentage);
+            Assert.AreEqual('C', grade);
+            Assert.AreEqual("Qualified", message);
+
+            // c - not qualifies
+            test = new Trainee(new string[] { "SQL", "C#" }, new float[] { 40, 35, 24 });
+            test.CalculateScore(out percentage, out grade, out message);
+            Assert.AreEqual(33, percentage);
+            Assert.AreEqual('C', grade);
+            Assert.AreEqual("Not Qualified", message);
+
+            
+
+            
+        }
+    }
+}

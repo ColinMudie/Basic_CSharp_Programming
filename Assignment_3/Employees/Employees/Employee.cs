0fef91307cc2f56ba02be9a0388d5f0611126035
Colin Mudie <csmudie1@gmail.com>
2022-02-15 10:49:44 -0500

    completed assignemnt 3

---
+++ Assignment_3/Employees/Employees/Employee.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    public class Employee
+    {
+        public DateTime DateOfBirth { get; set; }
+        public string[] Dependents { get; set; } = new string[3];
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public bool Gender { get; set; }
+        public short NumberOfDependents { get; set; }
+
+        private static int nextEmployeeNumber;
+
+        static Employee()
+        {
+            nextEmployeeNumber = 1000;
+        }
+
+        public Employee()
+        {
+            this.EmployeeId = ++nextEmployeeNumber;
+        }
+
+        public Employee(string p_employeeName, DateTime p_dateOfBirth, bool p_gender) :this()
+        {
+            this.EmployeeName = p_employeeName;
+            this.DateOfBirth = p_dateOfBirth;
+            this.Gender = p_gender;
+        }
+
+        public Employee(string p_employeeName, DateTime p_dateOfBirth, bool p_gender, short p_numberOfDependents) :this(p_employeeName, p_dateOfBirth, p_gender)
+        {
+            if (p_numberOfDependents < 0)
+            {
+                this.NumberOfDependents = 0;
+            }
+            else if (p_numberOfDependents > 3)
+            {
+                this.NumberOfDependents = 3;
+            }
+            else
+            {
+                this.NumberOfDependents = p_numberOfDependents;
+            }
+        }
+
+        public int AddDependent(string p_dependentName)
+        {
+            if (this.NumberOfDependents == 3)
+            {
+                return 0;
+            }
+            else
+            {
+                for (int i = 0; i < this.Dependents.Length; i++)
+                {
+                    if (this.Dependents[i] == null)
+                    {
+                        this.Dependents[i] = p_dependentName;
+                        return ++this.NumberOfDependents;
+                    }
+                }
+                return this.NumberOfDependents;
+            }
+        }
+
+        public bool UpdateDependents(string p_dependentsName, int p_dependentId)
+        {
+            if (this.Dependents[p_dependentId - 1] != null)
+            {
+                this.Dependents[p_dependentId - 1] = p_dependentsName;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Assignment_3/Employees/EmployeesTests/EmployeeTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Tests
+{
+    [TestClass()]
+    public class EmployeeTests
+    {
+        [TestMethod()]
+        public void EmployeeCtor3Test()
+        {
+            DateTime testDate = new DateTime(2000, 1, 1);
+            Employee test = new Employee("Gon", testDate, true);
+            Assert.AreEqual("Gon", test.EmployeeName);
+            Assert.AreEqual(testDate, test.DateOfBirth);
+            Assert.AreEqual(true, test.Gender);
+        }
+
+        [TestMethod()]
+        public void EmployeeCtor4Test()
+        {
+            DateTime testDate = new DateTime(2000, 1, 1);
+            Employee test = new Employee("Gon", testDate, true, 2);
+            Assert.AreEqual("Gon", test.EmployeeName);
+            Assert.AreEqual(testDate, test.DateOfBirth);
+            Assert.AreEqual(true, test.Gender);
+            Assert.AreEqual(2, test.NumberOfDependents);
+
+            test = new Employee("Gon", testDate, true, -2);
+            Assert.AreEqual(0, test.NumberOfDependents);
+
+            test = new Employee("Gon", testDate, true, 12);
+            Assert.AreEqual(3, test.NumberOfDependents);
+        }
+
+        [TestMethod()]
+        public void UpdateDependentsTest()
+        {
+            DateTime testDate = new DateTime(2000, 1, 1);
+            Employee test = new Employee("Gon", testDate, true, 2);
+            test.AddDependent("Name");
+
+            bool result = test.UpdateDependents("Updated", 1);
+            Assert.IsTrue(test.Dependents.Contains("Updated"));
+            Assert.AreEqual(true, result);
+
+            // dependent id out of reach
+            test = new Employee("Gon", testDate, true, 3);
+            test.AddDependent("Name");
+            result = test.UpdateDependents("fail", 3);
+            Assert.AreEqual(false, result);
+        }
+        
+        [TestMethod()]
+        public void AddDependentTest()
+        {
+            DateTime testDate = new DateTime(2000, 1, 1);
+            Employee test = new Employee("Gon", testDate, true, 2);
+
+            int result = test.AddDependent("Name");
+            Assert.IsTrue(test.Dependents.Contains("Name"));
+            Assert.AreEqual(3, result);
+
+            // 3 dependents already used up
+            test = new Employee("Gon", testDate, true, 3);
+            result = test.AddDependent("Name");
+            Assert.AreEqual(0, result);
+        }
+    }
+}

6cb02a4d084ec2924c3a9c0ca0a7498f2e1b84c5
Colin Mudie <csmudie1@gmail.com>
2022-02-15 14:44:30 -0500

    solved assignment 4

--- Assignment_4/JASTAP/JASTAP/Insurance.cs
+++ Assignment_4/JASTAP/JASTAP/Insurance.cs
@@ -24,7 +24,6 @@ namespace JASTAP
 
         public Insurance()
         {
-            this.InsuranceId = $"I{counter++}";
             this.Age = 18;
             this.CreditHistory = 45000;
         }
@@ -46,17 +45,77 @@ namespace JASTAP
         }
 
         /// <summary>
-        /// 
+        /// This method checks if the consumer is eligible to apply for 
+        /// insurance based on the following conditions:
+        ///     
+        ///     - If 'age' is less than 18, then the consumer is not eligible
+        ///     
+        ///     - If 'age' is greater than 18 and less than or equal to 30 and 'creditHistory' 
+        ///         is less than or equal to 60000, then the consumer is eligible
+        ///       
+        ///     - If 'age' is greater than 30 and 'creditHistory' less than or equal 45000, 
+        ///         then the consumer is eligible
         /// </summary>
-        /// <returns></returns>
+        /// <returns>should return 'True' if the consumer is eligible and 'False' otherwise.</returns>
         public bool CheckEligibility()
         {
-            return false;
+            if (this.Age < 18)
+            {
+                return false;
+            }
+            else if ((this.Age >= 18 && this.Age <= 30) && this.CreditHistory <= 60000)
+            {
+                return true;
+            }
+            else if (this.Age > 30 && this.CreditHistory <= 45000)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
+        /// <summary>
+        /// This method checks the documents submitted by the consumer against the set of 
+        /// 'acceptabledocuments' based on the following conditions:
+        /// 
+        ///     - ‘CheckEligibility’ method should be called to check if the 
+        ///         consumer is eligible for applying for insurance
+        ///         
+        ///     - If he is eligible, then the ‘documents’ submitted by the 
+        ///         consumer should match with the ‘acceptabledocuments’. 
+        ///         Even if one of the documents matches with the acceptable documents, 
+        ///         then the consumer is eligible
+        ///         
+        ///     - ‘InsuranceId’ for eligible consumer should be auto-generated inside 
+        ///         this method with the help of the variable ‘counter’. 
+        ///         The ‘InsuranceId’ should start with “I1000” and auto-incremented each time
+        /// </summary>
+        /// <param name="p_acceptableDocuments">array of documents to check</param>
+        /// <returns>should return 'True' if the consumer is eligible and 'False' otherwise.</returns>
         public bool CheckDocuments(string[] p_acceptableDocuments)
         {
-            return false;
+            if (this.CheckEligibility())
+            {
+                foreach (string acceptableDoc in p_acceptableDocuments)
+                {
+                    foreach (string doc in this.Documents)
+                    {
+                        if (doc == acceptableDoc)
+                        {
+                            this.InsuranceId = $"I{counter++}";
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
---
+++ Assignment_4/JASTAP/JASTAPTests/InsuranceTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JASTAP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JASTAP.Tests
+{
+    [TestClass()]
+    public class InsuranceTests
+    {
+        [TestMethod()]
+        public void CheckEligibilityTest()
+        {
+            // should pass
+            Insurance test = new Insurance("David", 50000, 23, new string[]{"doc1.pdf", "doc2.pdf" });
+            bool result = test.CheckEligibility();
+            Assert.IsTrue(result);
+
+            // should pass
+            test = new Insurance("Kyle", 40000, 32, new string[] { "doc1.pdf", "doc2.pdf" });
+            result = test.CheckEligibility();
+            Assert.IsTrue(result);
+
+            // fail less than 18 age.
+            test = new Insurance("David", 50000, 17, new string[] { "doc1.pdf", "doc2.pdf" });
+            result = test.CheckEligibility();
+            Assert.IsFalse(result);
+
+            // fail credit above 60,000 when age 18 > x >= 30
+            test = new Insurance("David", 70000, 23, new string[] { "doc1.pdf", "doc2.pdf" });
+            result = test.CheckEligibility();
+            Assert.IsFalse(result);
+
+            // fail credit above 45,000 when age > 30
+            test = new Insurance("David", 50000, 31, new string[] { "doc1.pdf", "doc2.pdf" });
+            result = test.CheckEligibility();
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod()]
+        public void CheckDocumentsTest()
+        {
+            Insurance test = new Insurance("David", 50000, 23, new string[] { "doc1.pdf", "doc2.pdf" });
+            bool result = test.CheckDocuments(new string[] { "document.pdf", "doc2.pdf" });
+            Assert.IsTrue(result);
+
+            // doc doesnt match
+            test = new Insurance("David", 50000, 23, new string[] { "doc1.pdf", "doc2.pdf" });
+            result = test.CheckDocuments(new string[] { "document.pdf", "ddocument2.pdf" });
+            Assert.IsFalse(result);
+
+            // eligibility failed
+            test = new Insurance("David", 80000, 23, new string[] { "doc1.pdf", "doc2.pdf" });
+            result = test.CheckDocuments(new string[] { "document.pdf", "doc2.pdf" });
+            Assert.IsFalse(result);
+        }
+    }
+}
